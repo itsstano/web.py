25dd4ef7e807345f176dc872ca164ea37d39000b
eMobilis <stanleyobiero800@gmail.com>
2023-07-11 08:34:15 +0300

    initial commit

---
+++ DjangoMIT/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request,"index.html")
+def about(request):
+    return render(request,"about.html")
+def service(request):
+    return render(request,"service.html")
+def project(request):
+    return render(request,"project.html")
+def contact(request):
+    return render(request,"contact.html")
+def testimonial(request):
+    return render(request,"testimonial.html")
